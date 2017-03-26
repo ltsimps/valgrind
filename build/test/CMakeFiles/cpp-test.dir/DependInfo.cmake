a079330f147f38fb72cac305471490ee59f5027e
ltsimps <ltsimpso@me.com>
2017-03-26 10:10:30 -0500

    Fixed two errors. One for unitialized value and one for non freed memory

---
+++ build/test/CMakeFiles/cpp-test.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/viki/workspace/valgrind/test/main.cpp" "/home/viki/workspace/valgrind/build/test/CMakeFiles/cpp-test.dir/main.cpp.o"
+  "/home/viki/workspace/valgrind/test/test.cpp" "/home/viki/workspace/valgrind/build/test/CMakeFiles/cpp-test.dir/test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/viki/workspace/valgrind/build/vendor/googletest/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../test/../vendor/googletest/googletest/include"
+  "../vendor/googletest/googletest/include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
